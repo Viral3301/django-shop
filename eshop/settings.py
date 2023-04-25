e9d13eb7c25929cf2b86727c464824fc5c160900
Viral3301 <okxolod2@gmail.com>
2023-04-25 21:49:09 +0300

    models and admin model added

--- eshop/settings.py
+++ eshop/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'eshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eshop',
+        'USER': 'postgres',
+        'PASSWORD': '223344',
+        'HOST': 'localhost'
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='product_image/')
+    manufacturer = models.CharField(max_length=100)
+    seats = models.PositiveIntegerField()
+    engine = models.PositiveIntegerField()
+    engine_type = models.CharField(max_length=100)
+    year = models.ImageField(max_length=100)
+    amount_in_stock = models.IntegerField()
+    sale = models.BooleanField()
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    slug = models.SlugField(max_length=200,default='',unique=True)
+
+    def __str__(self):
+        return self.name

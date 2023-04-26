81357d9c55c8d4dc7329e26f62c584db352e15d7
Viral3301 <okxolod2@gmail.com>
2023-04-26 22:57:57 +0300

    Home view added

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Product
 # Create your views here.
+
+class HomeView(ListView):
+    model = Product
+    paginate_by = 9
+    template_name = 'home.html'
